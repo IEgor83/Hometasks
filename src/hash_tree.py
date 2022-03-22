4190b72728c3a994323bd5ca8c7b47446589e432
Egor <e.r.ivanov@inbox.ru>
2022-03-22 12:49:07 +0400

    first commit, hashmap

---
+++ src/hash_tree.py
@@ -0,0 +1,13 @@
+class HashTree:
+    def __init__(self, key = None, value = None, left = None, right = None):
+        self.head = None
+        self.key = key
+        self.value = value
+        self.left = left
+        self.right = right
+    def __getitem__(self, key):
+        a =
+        def F(a):
+
+
+
