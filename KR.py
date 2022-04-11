1f64b5b31ce90005a75c9818700f642bee0be59c
Egor <e.r.ivanov@inbox.ru>
2022-04-11 19:29:15 +0400

    kontrolnaya#chaged

--- KR.py
+++ KR.py
@@ -8,6 +8,8 @@ class Number:
             self.number[0] = -self.number[0]
 
     def __add__(self, other):
+        if self.number[0] < 0 or other.number[0] < 0:
+            return self-(-other)
         if len(self.number) > len(other.number):
             max_mas = self.number
             min_mas = other.number
@@ -33,7 +35,39 @@ class Number:
         return Number(new_object)
 
     def __sub__(self, other):
-        ...
+        def difference(a, b):
+            r = len(a)-len(b)
+            for i in range(len(a)-1, -1, -1):
+                if (i-r) >= 0 and i > 0:
+                    if a[i] < b[i-r]:
+                        a[i - 1] -= 1
+                    a[i] = (a[i] - b[i-r]) % 10
+                elif i != 0:
+                    if a[i] < 0:
+                        a[i] = a[i] % 10
+                        a[i - 1] -= 1
+                    else:
+                        break
+                else:
+                    if r == 0:
+                        a[0] -= b[0]
+                    for elem in a:
+                        if elem == 0 and len(a) > 1:
+                            a.pop(0)
+                        else:
+                            break
+            return a
+        if self >= other and self.number[0] > 0:
+            if other.number[0] < 0:
+                return self + (-other)
+            new_obj = int(''.join(map(str, difference(self.number, other.number))))
+        elif self < other and other.number[0] > 0:
+            if self.number[0] < 0:
+                return -(other-self)
+            new_obj = -int(''.join(map(str, difference(other.number, self.number))))
+        else:
+            return (-other)-(-self)
+        return Number(new_obj)
 
     def __neg__(self):
         new_obj = self.number.copy()
@@ -44,10 +78,58 @@ class Number:
     def __eq__(self, other):
         if len(self.number) == len(other.number):
             for i in range(len(self.number)):
-                if self.number[0] != other.number[0]:
+                if self.number[i] != other.number[i]:
                     return False
             return True
         return False
+
+    def __lt__(self, other):
+        if len(self.number) == len(other.number):
+            for i in range(len(self.number)):
+                if self.number[i] < other.number[i]:
+                    return True
+                if self.number[i] > other.number[i]:
+                    return False
+            return False
+        return len(self.number) < len(other.number)
+
+    def __le__(self, other):
+        if len(self.number) == len(other.number):
+            for i in range(len(self.number)):
+                if self.number[i] < other.number[i]:
+                    return True
+                if self.number[i] > other.number[i]:
+                    return False
+            return True
+        return len(self.number) < len(other.number)
+
+    def __gt__(self, other):
+        if len(self.number) == len(other.number):
+            for i in range(len(self.number)):
+                if self.number[i] > other.number[i]:
+                    return True
+                if self.number[i] < other.number[i]:
+                    return False
+            return False
+        return len(self.number) > len(other.number)
+
+    def __ge__(self, other):
+        if len(self.number) == len(other.number):
+            for i in range(len(self.number)):
+                if self.number[i] > other.number[i]:
+                    return True
+                if self.number[i] < other.number[i]:
+                    return False
+            return True
+        return len(self.number) > len(other.number)
+
+    def __ne__(self, other):
+        if len(self.number) == len(other.number):
+            for i in range(len(self.number)):
+                if self.number[i] != other.number[i]:
+                    return True
+            return False
+        return True
 
     def __str__(self):
         return str(self.number)
@@ -56,6 +138,6 @@ class Number:
         return repr(self.number)
 
 
-s = Number(135789)
-p = Number(37645231)
-print(s == p)
+s = Number(-135789)
+p = Number(-37645231)
+print(p-s)
